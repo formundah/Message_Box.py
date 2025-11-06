44adeec03561d9fe38fbc58c72cd59b6e75ec98c
formundah <formundah@outlook.com>
2025-11-06 15:13:49 -0800

    Reformat MessageBox.py with consistent imports

--- MessageBox.py
+++ MessageBox.py
@@ -4,8 +4,8 @@ import ctypes
 message = "Hello, World!"
 title = "Simple Message Box"
 
+#for more documentation go to https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messagebox or MessageBoxW function (winuser.h).
 MB_OK = 0x00000000
 MB_ICONERROR = 0x00000030
-
 
 ctypes.windll.user32.MessageBoxW(0, message, title, MB_OK | MB_ICONERROR)
